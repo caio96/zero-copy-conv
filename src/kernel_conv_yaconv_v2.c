4d6df2756db53f2284b257e7a737df063a2b2688
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2024-10-20 15:42:05 -0600

    Add support to yaconv v2 kernel for stride > 1

--- src/kernel_conv_yaconv_v2.c
+++ src/kernel_conv_yaconv_v2.c
@@ -1,4 +1,5 @@
 #include "blis/blis.h"
+#include <math.h>
 
 // Function to print a matrix
 void print_matrix(float *matrix, int rows, int cols) {
@@ -17,8 +18,8 @@ void conv_2d_yaconv_v2_var1(float *__restrict__ input, float *__restrict__ outpu
                          int C, int FH, int FW, int M, int PH, int PW, int SH,
                          int SW) {
   // Output dimensions
-  const int OH = H + 2 * PH - FH + 1;
-  const int OW = W + 2 * PW - FW + 1;
+  const int OH = (H + 2 * PH - FH) / SH + 1;
+  const int OW = (W + 2 * PW - FW) / SW + 1;
 
   float *a, *b, *c;
   float *image_buffer =
@@ -38,29 +39,47 @@ void conv_2d_yaconv_v2_var1(float *__restrict__ input, float *__restrict__ outpu
 
       // Calculate height slice of size OH and handle edge cases
       int height_offset = fh - PH;
-      int height_start = bli_max(0, height_offset);
-      int height_end = bli_min(H, height_offset + OH);
-      int height_slice = height_end - height_start;
+      int height_start;
+      if (height_offset < 0) {
+        // Use modulo to handle negative values (python implementation of %)
+        height_start = bli_max(0, ((height_offset % SH) + SH) % SH);
+      } else {
+        height_start = bli_max(0, height_offset);
+      }
+      int height_end = bli_min(H, height_offset + OH * SH);
+      int height_slice = ceilf((height_end - height_start) / (float) SH);
+
+      // Print height variables
+      // printf("\nheight_offset: %d\n", height_offset);
+      // printf("height_start: %d\n", height_start);
+      // printf("height_end: %d\n", height_end);
+      // printf("height_slice: %d\n", height_slice);
+
+      if (height_slice <= 0)
+        continue;
 
       // For every output width element
       for (int ow = 0; ow < OW; ++ow) {
 
         // Calculate width slice of size FW and handle edge cases
-        int ow_offset = ow - PW;
-        int width_start = bli_max(0, ow_offset);
-        int width_end = bli_min(W, ow_offset + FW);
+        int iw = ow * SH - PW;
+        int width_start = bli_max(0, iw);
+        int width_end = bli_min(W, iw + FW);
         int width_slice = width_end - width_start;
 
+        if (width_slice <= 0)
+          continue;
+
         // Start of the filter block of size 1,FW,C,M
-        if (ow_offset < 0) {
-          b = &filters[fh * FW * C * M - ow_offset * C * M];
+        if (iw < 0) {
+          b = &filters[fh * FW * C * M - iw * C * M];
         } else {
           b = &filters[fh * FW * C * M];
         }
 
         // Copy input slice to image buffer
         int buf_index = 0;
-        for (int h = height_start; h < height_end; ++h) {
+        for (int h = height_start; h < height_end; h+=SH) {
           for (int w = width_start; w < width_end; ++w) {
             for (int c = 0; c < C; ++c) {
               image_buffer[buf_index++] = single_input[h * W * C + w * C + c];
@@ -73,7 +92,8 @@ void conv_2d_yaconv_v2_var1(float *__restrict__ input, float *__restrict__ outpu
 
         // Start of the output block of size 1,OH,M
         if (height_offset < 0) {
-          c = &output_buffer[ow * OH * M - height_offset * M];
+          int offset = floorf(height_offset/(float)SH);
+          c = &output_buffer[ow * OH * M - offset * M];
         } else {
           c = &output_buffer[ow * OH * M];
         }
@@ -83,6 +103,12 @@ void conv_2d_yaconv_v2_var1(float *__restrict__ input, float *__restrict__ outpu
         int N_dim = M;
         float alpha = 1.0f;
         float beta = 1.0f;
+
+        // print A and B
+        // printf("\nA\n");
+        // print_matrix(a, M_dim, K_dim);
+        // printf("B\n");
+        // print_matrix(b, K_dim, N_dim);
 
         bli_sgemm(BLIS_NO_TRANSPOSE, BLIS_NO_TRANSPOSE, M_dim, N_dim, K_dim,
                   &alpha, a, K_dim, 1, b, N_dim, 1, &beta, c, N_dim, 1);
