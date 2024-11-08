c7b849e66152734c1dba4922707fedc53ff8fb79
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2024-11-07 21:53:10 -0700

    Rearrange zero_copy loops and add multithreading

--- src/kernel_conv_zero_copy.c
+++ src/kernel_conv_zero_copy.c
@@ -1,56 +1,58 @@
 #include "blis/blis.h"
 #include <math.h>
+#include <omp.h>
 
 int modulo(int a, int b) {
   const int result = a % b;
   return result >= 0 ? result : result + b;
 }
 
-// Input is in NHWC format
-// Filters are in HWIO format
-// Output is in NWHC format (height and width are transposed)
-void conv_2d_zero_copy(float *__restrict__ input,
-                               float *__restrict__ output,
-                               float *__restrict__ filters, int N, int H, int W,
-                               int C, int FH, int FW, int OH, int OW, int M,
-                               int PH, int PW, int SH, int SW) {
-  float *a, *b, *c;
+// Performs convolution 2D with zero copies other than the packings that may be
+// used in BLIS.
+// - Input is in NHWC format
+// - Filters are in HWIO format
+// - Output is in NWHC format (height and width are transposed)
+void conv_2d_zero_copy(float *__restrict__ input, float *__restrict__ output,
+                       float *__restrict__ filters, int N, int H, int W, int C,
+                       int FH, int FW, int OH, int OW, int M, int PH, int PW,
+                       int SH, int SW) {
+  // Initialize output to zeros
+  bli_ssetv(BLIS_NO_CONJUGATE, N * OH * OW * M, bli_s0, output, 1);
 
+#pragma omp parallel for collapse(2)
   // For every batch element
   for (int n = 0; n < N; ++n) {
-    float *single_input = &input[n * H * W * C];
-    float *single_output = &output[n * OH * OW * M];
+    // For every output width element
+    for (int ow = 0; ow < OW; ++ow) {
 
-    // Initialize output to zeros
-    bli_ssetv(BLIS_NO_CONJUGATE, OH * OW * M, bli_s0, single_output, 1);
+      float *single_input = &input[n * H * W * C];
+      float *single_output = &output[n * OH * OW * M];
+      float *a, *b, *c;
 
-    // For every element in the filter height
-    for (int fh = 0; fh < FH; ++fh) {
+      // Calculate width slice of size FW and handle edge cases
+      int iw = ow * SW - PW;
+      int width_start = bli_max(0, iw);
+      int width_end = bli_min(W, iw + FW);
+      int width_slice = width_end - width_start;
 
-      // Calculate height slice of size OH and handle edge cases
-      int height_offset = fh - PH;
-      int height_start;
-      if (height_offset < 0) {
-        height_start = bli_max(0, modulo(height_offset, SH));
-      } else {
-        height_start = bli_max(0, height_offset);
-      }
-      int height_end = bli_min(H, height_offset + OH * SH);
-      int height_slice = ceilf((height_end - height_start) / (float)SH);
-
-      if (height_slice <= 0)
+      if (width_slice <= 0)
         continue;
 
-      // For every output width element
-      for (int ow = 0; ow < OW; ++ow) {
+      // For every element in the filter height
+      for (int fh = 0; fh < FH; ++fh) {
 
-        // Calculate width slice of size FW and handle edge cases
-        int iw = ow * SW - PW;
-        int width_start = bli_max(0, iw);
-        int width_end = bli_min(W, iw + FW);
-        int width_slice = width_end - width_start;
+        // Calculate height slice of size OH and handle edge cases
+        int height_offset = fh - PH;
+        int height_start;
+        if (height_offset < 0) {
+          height_start = bli_max(0, modulo(height_offset, SH));
+        } else {
+          height_start = bli_max(0, height_offset);
+        }
+        int height_end = bli_min(H, height_offset + OH * SH);
+        int height_slice = ceilf((height_end - height_start) / (float)SH);
 
-        if (width_slice <= 0)
+        if (height_slice <= 0)
           continue;
 
         // Start of the filter block of size 1,FW,C,M
@@ -84,67 +86,72 @@ void conv_2d_zero_copy(float *__restrict__ input,
   }
 }
 
-// Input is in NHWC format
-// Filters are in HWIO format
-// Output is in NWHC format (height and width are transposed)
-// This version has additional support for dilated convolution, which requires
-// an extra buffer of size OH*FW*C.
+// This version performs convolution 2D has additional support for dilated
+// convolution, which requires an extra buffer of size OH*FW*(C/GR) for every
+// thread used.
+//
+// - Input is in NHWC format
+// - Filters are in HWIO format
+// - Output is in NWHC format (height and width are transposed)
 void conv_2d_zero_copy_ext(float *__restrict__ input,
-                            float *__restrict__ output,
-                            float *__restrict__ filters, int N, int H, int W,
-                            int C, int FH, int FW, int OH, int OW, int M,
-                            int PH, int PW, int SH, int SW, int DH, int DW,
-                            int GR) {
-  float *a, *b, *c;
-  int C_GR = C / GR;
-  int M_GR = M / GR;
-  float *packed_image = aligned_alloc(64, OH * FW * C_GR * sizeof(float));
+                           float *__restrict__ output,
+                           float *__restrict__ filters, int N, int H, int W,
+                           int C, int FH, int FW, int OH, int OW, int M, int PH,
+                           int PW, int SH, int SW, int DH, int DW, int GR) {
+  // Compute channel groupings
+  const int C_GR = C / GR;
+  const int M_GR = M / GR;
 
+  // Initialize output to zeros
+  bli_ssetv(BLIS_NO_CONJUGATE, N * OH * OW * M, bli_s0, output, 1);
+
+#pragma omp parallel for collapse(2)
   // For every batch element
   for (int n = 0; n < N; ++n) {
-    float *single_input = &input[n * H * W * C];
-    float *single_output = &output[n * OH * OW * M];
+    // For every output width element
+    for (int ow = 0; ow < OW; ++ow) {
 
-    // Initialize output to zeros
-    bli_ssetv(BLIS_NO_CONJUGATE, OH * OW * M, bli_s0, single_output, 1);
+      float *single_input = &input[n * H * W * C];
+      float *single_output = &output[n * OH * OW * M];
+      float *a, *b, *c;
+      float *packed_image = aligned_alloc(64, OH * FW * C_GR * sizeof(float));
 
-    // For every element in the filter height
-    for (int fh = 0; fh < FH; ++fh) {
-
-      // Calculate height slice of size OH and handle edge cases
-      int height_offset = fh * DH - PH;
-      int height_start;
-      if (height_offset < 0) {
-        height_start = bli_max(0, modulo(height_offset, SH));
+      // Calculate width slice of size FW and handle edge cases
+      int iw = ow * SW - PW;
+      int width_start;
+      if (iw < 0) {
+        width_start = bli_max(0, modulo(iw, DW));
       } else {
-        height_start = bli_max(0, height_offset);
+        width_start = bli_max(0, iw);
       }
-      int height_end = bli_min(H, height_offset + OH * SH);
-      int height_slice = ceilf((height_end - height_start) / (float)SH);
+      int width_end = bli_min(W, iw + FW * DW);
+      int width_slice = ceilf((width_end - width_start) / (float)DW);
 
-      if (height_slice <= 0)
+      if (width_slice <= 0)
         continue;
 
-      // For every output width element
-      for (int ow = 0; ow < OW; ++ow) {
+      // For every element in the filter height
+      for (int fh = 0; fh < FH; ++fh) {
 
-        // Calculate width slice of size FW and handle edge cases
-        int iw = ow * SW - PW;
-        int width_start;
-        if (iw < 0) {
-          width_start = bli_max(0, modulo(iw, DW));
+        // Calculate height slice of size OH and handle edge cases
+        int height_offset = fh * DH - PH;
+        int height_start;
+        if (height_offset < 0) {
+          height_start = bli_max(0, modulo(height_offset, SH));
         } else {
-          width_start = bli_max(0, iw);
+          height_start = bli_max(0, height_offset);
         }
-        int width_end = bli_min(W, iw + FW * DW);
-        int width_slice = ceilf((width_end - width_start) / (float)DW);
+        int height_end = bli_min(H, height_offset + OH * SH);
+        int height_slice = ceilf((height_end - height_start) / (float)SH);
 
-        if (width_slice <= 0)
+        if (height_slice <= 0)
           continue;
 
+        // For every group of channels
         for (int gr = 0; gr < GR; ++gr) {
 
-          // Copy input slice to image buffer
+          // Copy input slice to image buffer following stride
+          // height, width dilation, and channel grouping
           int buf_index = 0;
           for (int h = height_start; h < height_end; h += SH) {
             for (int w = width_start; w < width_end; w += DW) {
@@ -155,7 +162,7 @@ void conv_2d_zero_copy_ext(float *__restrict__ input,
             }
           }
 
-          // Start of the filter block of size 1,FW,C,M
+          // Start of the filter block of size 1,FW,C_GR,M
           if (iw < 0) {
             int adjusted_iw = floorf(iw / (float)DW);
             b = &filters[fh * FW * C_GR * M - adjusted_iw * C_GR * M +
@@ -164,7 +171,7 @@ void conv_2d_zero_copy_ext(float *__restrict__ input,
             b = &filters[fh * FW * C_GR * M + gr * M_GR];
           }
 
-          // Start of the image block of size OH,FW,C
+          // Start of the image block of size OH,FW,C_GR
           a = packed_image;
 
           // Start of the output block of size 1,OH,M
@@ -185,21 +192,22 @@ void conv_2d_zero_copy_ext(float *__restrict__ input,
                     &alpha, a, K_dim, 1, b, M, 1, &beta, c, M, 1);
         }
       }
+      free(packed_image);
     }
   }
-
-  free(packed_image);
 }
 
-void conv_2d_zero_copy_main(float *__restrict__ input, float *__restrict__ output,
-                       float *__restrict__ filters, int N, int H, int W, int C,
-                       int FH, int FW, int OH, int OW, int M, int PH, int PW,
-                       int SH, int SW, int DH, int DW, int GR) {
+void conv_2d_zero_copy_main(float *__restrict__ input,
+                            float *__restrict__ output,
+                            float *__restrict__ filters, int N, int H, int W,
+                            int C, int FH, int FW, int OH, int OW, int M,
+                            int PH, int PW, int SH, int SW, int DH, int DW,
+                            int GR) {
   if (DH == 1 && DW == 1 && GR == 1) {
-    conv_2d_zero_copy(input, output, filters, N, H, W, C, FH, FW, OH,
-                              OW, M, PH, PW, SH, SW);
+    conv_2d_zero_copy(input, output, filters, N, H, W, C, FH, FW, OH, OW, M, PH,
+                      PW, SH, SW);
   } else {
-    conv_2d_zero_copy_ext(input, output, filters, N, H, W, C, FH, FW, OH, OW,
-                           M, PH, PW, SH, SW, DH, DW, GR);
+    conv_2d_zero_copy_ext(input, output, filters, N, H, W, C, FH, FW, OH, OW, M,
+                          PH, PW, SH, SW, DH, DW, GR);
   }
 }
