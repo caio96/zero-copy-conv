16863f41b47e9b0877b11403c17f12339ebee30a
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2024-11-10 16:22:14 -0700

    Add threads and core-range to benchmark_runner

--- scripts/benchmark_runner.sh
+++ scripts/benchmark_runner.sh
@@ -1,50 +1,32 @@
-#!/bin/bash
-# Run executables and generate a log with execution times
-
-# Gets the location of the script
-function getScriptLocation {
-    SOURCE="${BASH_SOURCE[0]}"
-    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-        SOURCE="$(readlink "$SOURCE")"
-        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-    done
-        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-        echo "$DIR"
-}
+#!/usr/bin/env bash
 
 function Help()
 {
-   echo "Run executables in build folder with the parameters defined in a csv file and generate an output log with performance metrics"
+   echo "Run executables in build folder with the parameters defined in a csv file and generate an output log with performance or correctness metrics"
    echo
    echo "Syntax: $(basename "$0") BUILD_DIR CSV_FILE OUTPUT_FILE"
    echo "Options:"
    echo -e "\t--append-output: Allow output to be appended to existing file."
    echo -e "\t--batch-size [size]: Specify a custom batch size. Default is 1."
    echo -e "\t--repeats [repeats]: Specify the number of times each benchmark is repeated. Default is 1."
+   echo -e "\t--threads [threads]: Specify the number of threads that each run can use (sets OMP_NUM_THREADS). Default is 1."
+   echo -e "\t--core-range [range]: Specify the range of cores that a run may use. For example, to use only the first 12 cores, set it to '1-12'. Useful to select performance cores. Default is to use all cores."
    echo -e "\t--check-correctness: Run each benchmark once to check correctness. Ignores --repeats and --append-output."
    echo -e "\t-h: Print this Help."
    echo -e "\t-v: Verbose mode."
    echo
 }
 
-function CheckPerfParanoid()
-{
-  paranoid=$(cat '/proc/sys/kernel/perf_event_paranoid')
-  if [ "$paranoid" -gt "1" ]; then
-    echo "Please set kernel event paranoid to a least 1 to be able to use perf"
-    echo "Use the following command:"
-    echo "    sudo sh -c 'echo 1 > /proc/sys/kernel/perf_event_paranoid'"
-    exit 1
-  fi
-}
-
 BATCH_SIZE="1"
 REPEATS="1"
 APPEND_OUTPUT="false"
 CHECK_CORRECTNESS="false"
+OMP_NUM_THREADS="1"
+# Get list of cores from numactl, remove trailing spaces and get last number
+CORE_RANGE="0-$(numactl --show | grep "physcpubind" | sed 's/[[:blank:]]*$//' | tr -s ' ' | rev | cut -d ' ' -f 1 | rev)"
+
 # Parse Arguments
-PARSED_ARGUMENTS=$(getopt -a -n "benchmark_runner" -o hv --long append-output,check-correctness,repeats:,batch-size: -- "$@")
+PARSED_ARGUMENTS=$(getopt -a -n "benchmark_runner" -o hv --long append-output,check-correctness,repeats:,batch-size:,threads:,core-range: -- "$@")
 if [ $? -ne 0 ]; then
   echo "Invalid option." >&2
   Help
@@ -69,9 +51,17 @@ while true; do
       BATCH_SIZE="$2"
       shift 2
       ;;
+    --threads)
+      OMP_NUM_THREADS="$2"
+      shift 2
+      ;;
     --append-output)
       APPEND_OUTPUT="true"
       shift
+      ;;
+    --core-range)
+      CORE_RANGE="$2"
+      shift 2
       ;;
     --check-correctness)
       CHECK_CORRECTNESS="true"
@@ -127,9 +117,6 @@ if [[ -n $ERROR_ARGUMENT ]]; then
   exit 1
 fi
 
-# Check if perf will work
-CheckPerfParanoid
-
 # Create list with all executables found in build folder that start with "benchmark_"
 executables=()
 for executable in $(find "$BUILD_DIR" -type f -name "benchmark_*" | sort); do
@@ -158,50 +145,48 @@ export BENCHMARK_FORMAT="csv"
 
 # Add header to output log if not appending
 if [[ "$APPEND_OUTPUT" == "false" ]] && [[ "$CHECK_CORRECTNESS" == "false" ]]; then
-  "${executables[0]}" 2> /dev/null | head -n1 | tee -a "$OUTPUT_LOG"
+  "${executables[0]}" 2> /dev/null | head -n1 >> "$OUTPUT_LOG"
 fi
 
 # Add header to correctness output
 if [[ "$CHECK_CORRECTNESS" == "true" ]]; then
-  $CORRECTNESS_EXECUTABLE | head -n1 | tee -a "$OUTPUT_LOG"
+  $CORRECTNESS_EXECUTABLE | head -n1 >> "$OUTPUT_LOG"
 fi
+
+# Set number of threads
+export OMP_NUM_THREADS
+
+echo -e "Running with $REPEATS repetitions and $OMP_NUM_THREADS threads, using cores $CORE_RANGE\n"
 
 # For each repetition
 for repeat in $(seq "$REPEATS"); do
   echo "Starting repetition $repeat of $REPEATS --------------------"
 
   # For each configuration in the csv file
-  while IFS=',' read -r id batch ic ih iw oc oh ow fh hw pt pb pl pr sh sw dh dw gr; do
+  while IFS=',' read -r conv_parameters occurrences models; do
     # Skip header
-    if [[ "$id" == "ID" ]]; then
+    if [[ "$conv_parameters" == "conv_parameters" ]]; then
       continue
     fi
 
-    echo ""
-    echo "Running benchmark for ID: $id"
-    echo "  - Batch: $BATCH_SIZE"
-    echo "  - Input: $ic x $ih x $iw"
-    echo "  - Output: $oc x $oh x $ow"
-    echo "  - Filter: $fh x $hw"
-    echo "  - Padding: $pt $pb $pl $pr"
-    echo "  - Stride: $sh $sw"
-    echo "  - Dilation: $dh $dw"
-    echo "  - Groups: $gr"
+    # Add custom batch size to parameters
+    conv_parameters="$BATCH_SIZE $(echo "$conv_parameters" | cut -d ' ' -f2-)"
+
+    echo "Running $conv_parameters"
 
     # Check correctness
     if [[ "$CHECK_CORRECTNESS" == "true" ]]; then
-      "$CORRECTNESS_EXECUTABLE" "$BATCH_SIZE" "$ic" "$ih" "$iw" "$oc" "$oh" "$ow" "$fh" "$hw" "$pt" "$pb" "$pl" "$pr" "$sh" "$sw" "$dh" "$dw" "$gr" | tail -n +2 | tee -a "$OUTPUT_LOG"
+      "$CORRECTNESS_EXECUTABLE" "$conv_parameters" | tail -n +2 | tee -a "$OUTPUT_LOG"
       continue
     fi
 
     # For each executable (shuffled order)
     for executable in $(shuf -e "${executables[@]}"); do
-      # Get random number
-      CORE_NUM=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
+      # Get random core list within range of size OMP_NUM_THREADS
+      CORES=$(shuf -i "$CORE_RANGE" -n "$OMP_NUM_THREADS" | tr '\n' ',' | sed 's/,$//')
       # Run executable in a random core
-      numactl --physcpubind $(( $RANDOM % $CORE_NUM )) "$executable" "$BATCH_SIZE" "$ic" "$ih" "$iw" "$oc" "$oh" "$ow" "$fh" "$hw" "$pt" "$pb" "$pl" "$pr" "$sh" "$sw" "$dh" "$dw" "$gr" 2> /dev/null | tail -n +2 | tee -a "$OUTPUT_LOG"
+      numactl --physcpubind "$CORES" "$executable" ${conv_parameters} 2> /dev/null | tail -n +2 | tee -a "$OUTPUT_LOG"
     done
 
   done < "$CSV_FILE"
 done
-
