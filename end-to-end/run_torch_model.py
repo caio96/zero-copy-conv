7c044b61953bb794a0ed48f296c03bf6197695ab
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2024-12-17 18:19:50 -0700

    Update pytorch model runner

--- end-to-end/run_torch_model.py
+++ end-to-end/run_torch_model.py
@@ -7,33 +7,33 @@ import torchvision.models as models
 
 def run_model(model_name):
     if model_name == "resnet18":
-        model = models.resnet18(weights=True)
+        model = models.resnet18()
     elif model_name == "alexnet":
-        model = models.alexnet(weights=True)
+        model = models.alexnet()
     elif model_name == "squeezenet":
-        model = models.squeezenet1_0(weights=True)
+        model = models.squeezenet1_0()
     elif model_name == "vgg16":
-        model = models.vgg16(weights=True)
+        model = models.vgg16()
     elif model_name == "densenet":
-        model = models.densenet161(weights=True)
+        model = models.densenet161()
     elif model_name == "inception":
-        model = models.inception_v3(weights=True)
+        model = models.inception_v3()
     elif model_name == "googlenet":
-        model = models.googlenet(weights=True)
+        model = models.googlenet()
     elif model_name == "shufflenet":
-        model = models.shufflenet_v2_x1_0(weights=True)
+        model = models.shufflenet_v2_x1_0()
     elif model_name == "mobilenet_v2":
-        model = models.mobilenet_v2(weights=True)
+        model = models.mobilenet_v2()
     elif model_name == "mobilenet_v3_large":
-        model = models.mobilenet_v3_large(weights=True)
+        model = models.mobilenet_v3_large()
     elif model_name == "mobilenet_v3_small":
-        model = models.mobilenet_v3_small(weights=True)
+        model = models.mobilenet_v3_small()
     elif model_name == "resnext50_32x4d":
-        model = models.resnext50_32x4d(weights=True)
+        model = models.resnext50_32x4d()
     elif model_name == "wide_resnet50_2":
-        model = models.wide_resnet50_2(weights=True)
+        model = models.wide_resnet50_2()
     elif model_name == "mnasnet":
-        model = models.mnasnet1_0(weights=True)
+        model = models.mnasnet1_0()
     else:
         raise ValueError(f"Model {model_name} is not supported.")
 
@@ -50,27 +50,27 @@ def run_model(model_name):
         output = model(dummy_input)
         end = time.time()
         elapsed_time_ms = (end - start) * 1000
-        print(f"Inference of ONNX model used {elapsed_time_ms:.4f} ms")
+        print(f"Inference of model used {elapsed_time_ms:.4f} ms")
         start = time.time()
         output = model(dummy_input)
         end = time.time()
         elapsed_time_ms = (end - start) * 1000
-        print(f"Inference of ONNX model used {elapsed_time_ms:.4f} ms")
+        print(f"Inference of model used {elapsed_time_ms:.4f} ms")
         start = time.time()
         output = model(dummy_input)
         end = time.time()
         elapsed_time_ms = (end - start) * 1000
-        print(f"Inference of ONNX model used {elapsed_time_ms:.4f} ms")
+        print(f"Inference of model used {elapsed_time_ms:.4f} ms")
         start = time.time()
         output = model(dummy_input)
         end = time.time()
         elapsed_time_ms = (end - start) * 1000
-        print(f"Inference of ONNX model used {elapsed_time_ms:.4f} ms")
+        print(f"Inference of model used {elapsed_time_ms:.4f} ms")
         start = time.time()
         output = model(dummy_input)
         end = time.time()
         elapsed_time_ms = (end - start) * 1000
-        print(f"Inference of ONNX model used {elapsed_time_ms:.4f} ms")
+        print(f"Inference of model used {elapsed_time_ms:.4f} ms")
 
 
 if __name__ == "__main__":
