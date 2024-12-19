b7e2104e377ac835f1db7416b860b6314cec19c3
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2024-12-19 16:15:55 -0700

    Update run_torch_model to use all available models and take arguments

--- end-to-end/run_torch_model.py
+++ end-to-end/run_torch_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import time
+import os
 
 import torch
 import torchvision.models as models
@@ -10,45 +11,21 @@ def run_inference(model, input):
     with torch.no_grad():  # Disable gradient calculation
         return model(input)
 
-def run_model(model_name):
-    if model_name == "resnet18":
-        model = models.resnet18()
-    elif model_name == "alexnet":
-        model = models.alexnet()
-    elif model_name == "squeezenet":
-        model = models.squeezenet1_0()
-    elif model_name == "vgg16":
-        model = models.vgg16()
-    elif model_name == "densenet":
-        model = models.densenet161()
-    elif model_name == "inception":
-        model = models.inception_v3()
-    elif model_name == "googlenet":
-        model = models.googlenet()
-    elif model_name == "shufflenet":
-        model = models.shufflenet_v2_x1_0()
-    elif model_name == "mobilenet_v2":
-        model = models.mobilenet_v2()
-    elif model_name == "mobilenet_v3_large":
-        model = models.mobilenet_v3_large()
-    elif model_name == "mobilenet_v3_small":
-        model = models.mobilenet_v3_small()
-    elif model_name == "resnext50_32x4d":
-        model = models.resnext50_32x4d()
-    elif model_name == "wide_resnet50_2":
-        model = models.wide_resnet50_2()
-    elif model_name == "mnasnet":
-        model = models.mnasnet1_0()
-    else:
-        raise ValueError(f"Model {model_name} is not supported.")
+def run_model(model_name, compile=False):
+    # Load model with default weights
+    weights = models.get_model_weights(model_name).DEFAULT
+    model = models.get_model(model_name, weights=weights)
 
-    # Create a dummy input tensor with the shape that the model expects (batch_size, channels, height, width)
-    dummy_input = torch.randn(1, 3, 224, 224)
+    # Pre process input with the model's transforms
+    dummy_input = torch.randn(3, 224, 224)
+    preprocess = weights.transforms()
+    dummy_input = preprocess(dummy_input).unsqueeze(0)
 
     model.eval()  # Set the model to evaluation mode
-    model = model.to(memory_format=torch.channels_last)  # Replace with your model
-    dummy_input = dummy_input.to(memory_format=torch.channels_last)  # Replace with your input tensor
-    # model = torch.compile(model)
+    model = model.to(device="cpu", memory_format=torch.channels_last)  # Replace with your model
+    dummy_input = dummy_input.to(device="cpu", memory_format=torch.channels_last)  # Replace with your input tensor
+    if (compile):
+        model = torch.compile(model)
 
     num_threads = torch.get_num_threads()
 
@@ -57,8 +34,7 @@ def run_model(model_name):
         num_threads=num_threads,
         setup='from __main__ import run_inference',
         globals={'model': model, 'input': dummy_input})
-    # print(t0.timeit(10))
-    m0 = t0.blocked_autorange(min_run_time=1)
+    m0 = t0.blocked_autorange(min_run_time=0.5)
     print(m0)
 
 
@@ -66,15 +42,64 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Run a PyTorch model."
     )
+    all_models = models.list_models(module=models)
     parser.add_argument(
         "model_name",
         type=str,
-        choices=[
-            "resnet18", "alexnet", "squeezenet", "vgg16", "densenet", "inception",
-            "googlenet", "shufflenet", "mobilenet_v2", "mobilenet_v3_large",
-            "mobilenet_v3_small", "resnext50_32x4d", "wide_resnet50_2", "mnasnet"
-        ],
+        choices=all_models,
         help="The name of the model to convert.",
     )
+
+    parser.add_argument(
+        "--compile",
+        action="store_true",
+        help="Use torch.compile to compile the model.",
+    )
+
+    parser.add_argument(
+        "--enable-zero-copy-conv",
+        action="store_true",
+        help="Enable Zero Copy Conv in Pytorch.",
+    )
+
+    parser.add_argument(
+        "--show-conv-time",
+        action="store_true",
+        help="Print the execution time of each convolution in the model.",
+    )
+
+    parser.add_argument(
+        "--ignore-weight-transform",
+        action="store_true",
+        help="Make Zero Copy Conv ignore the weight transformation required for correct results (OHWI -> HWIO).",
+    )
+
+    parser.add_argument(
+        "--ignore-output-transform",
+        action="store_true",
+        help="Make Zero Copy Conv ignore the output transformation required for correct results (NWHC -> NHWC).",
+    )
+
     args = parser.parse_args()
-    run_model(args.model_name)
+
+    if args.enable_zero_copy_conv:
+        os.environ["ZERO_COPY_2D"] = "TRUE"
+    else:
+        os.environ["ZERO_COPY_2D"] = "FALSE"
+
+    if args.show_conv_time:
+        os.environ["SHOW_CONV_TIME"] = "TRUE"
+    else:
+        os.environ["SHOW_CONV_TIME"] = "FALSE"
+
+    if args.ignore_weight_transform:
+        os.environ["ZERO_COPY_TRANSFORM_WEIGHTS"] = "FALSE"
+    else:
+        os.environ["ZERO_COPY_TRANSFORM_WEIGHTS"] = "TRUE"
+
+    if args.ignore_output_transform:
+        os.environ["ZERO_COPY_TRANSFORM_OUTPUT"] = "FALSE"
+    else:
+        os.environ["ZERO_COPY_TRANSFORM_OUTPUT"] = "TRUE"
+
+    run_model(args.model_name, args.compile)
