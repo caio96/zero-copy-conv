d0befdccf8db5bd8458db8adcac3f8849a68e891
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2025-01-10 16:53:22 -0700

    Split summarize_performance into two scripts

---
+++ single-conv/scripts/graph-performance.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+import itertools
+from math import ceil, floor, log10
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from filter_csv import exclude_from_df, include_only_in_df, split_parameters
+
+
+def get_speedup(joined_results: pd.DataFrame, old_method_name, new_method_name):
+
+    # Remove rows where an error occurred in either method
+    joined_results = joined_results.loc[
+        (joined_results["error_occurred_" + old_method_name] == False)
+        & (joined_results["error_occurred_" + new_method_name] == False)
+    ]
+
+    speedup_results = pd.DataFrame()
+    speedup_results["conv_parameters"] = joined_results["conv_parameters"]
+
+    # Compute speedup
+    speedup_results["speedup"] = (
+        joined_results["mean_time_" + old_method_name]
+        - joined_results["mean_time_" + new_method_name]
+    ) / joined_results["mean_time_" + new_method_name]
+    speedup_results = speedup_results.sort_values(by="speedup", ascending=False)
+
+    return speedup_results
+
+
+# Saves a csv with results and produces an speedup graph
+def compare_methods(joined_results: pd.DataFrame, old_method_name, new_method_name):
+
+    speedup_results = get_speedup(joined_results, old_method_name, new_method_name)
+
+    # Save resulst to csv
+    speedup_results.to_csv(
+        output_dir / f"conv2d_{new_method_name}_vs_{old_method_name}.csv", index=False
+    )
+
+    speedup = speedup_results["speedup"]
+    num_points = speedup.shape[0]
+
+    inflection = num_points
+    for i in range(0, num_points - 1):
+        if speedup.iloc[i] > 0 and speedup.iloc[i + 1] < 0:
+            inflection = i + 0.5
+
+    pos = speedup.loc[lambda x: x >= 0].reset_index(drop=True)
+    neg = speedup.loc[lambda x: x < 0].reset_index(drop=True)
+
+    fig, ax = plt.subplots()
+
+    # barplot
+    ax.bar(pos.index, pos, color="#2c7bb6")
+    ax.bar(range(pos.shape[0], pos.shape[0] + neg.shape[0], 1), neg.values, color="#d7191c")
+
+    # boxplot
+    _, x_max = ax.get_xlim()
+    ax.set_xlim((-x_max * 0.05, num_points + x_max * 0.05))
+    ax.boxplot(
+        [pos, neg],
+        showfliers=False,
+        positions=[-x_max * 0.025, num_points + x_max * 0.025],
+        widths=x_max * 0.02,
+    )
+
+    ax.set_ylabel("Speedup/Slowdown")
+    ax.set_xlabel("Convolutions Layers")
+    ax.set_xticks([0, inflection, num_points], [0, int(inflection), num_points])
+
+    y_factor = 0.1
+    y_min, y_max = ax.get_ylim()
+    if y_min < 0:
+        relative_y = min(-y_min * y_factor, y_max * y_factor)
+    else:
+        relative_y = y_max * y_factor
+
+    y_total = y_max - y_min
+
+    ax.hlines(-y_total * 0.05, 1, inflection, "#2c7bb6")
+    ax.vlines(1, -y_total * 0.05 - y_total * 0.01, -y_total * 0.05 + y_total * 0.01, "#2c7bb6")
+    ax.vlines(
+        inflection,
+        -y_total * 0.05 - y_total * 0.01,
+        -y_total * 0.05 + y_total * 0.01,
+        "#2c7bb6",
+    )
+    ax.text(
+        (inflection / 2),
+        -y_total * 0.08,
+        f"{pos.shape[0]}",
+        horizontalalignment="center",
+        verticalalignment="center",
+    )
+
+    if neg.shape[0] != 0:
+        ax.hlines(y_total * 0.05, inflection, num_points, "#d7191c")
+        ax.vlines(
+            inflection,
+            y_total * 0.05 - y_total * 0.01,
+            y_total * 0.05 + y_total * 0.01,
+            "#d7191c",
+        )
+        ax.vlines(
+            num_points,
+            y_total * 0.05 - y_total * 0.01,
+            y_total * 0.05 + +y_total * 0.01,
+            "#d7191c",
+        )
+        ax.text(
+            ((num_points + inflection) / 2),
+            y_total * 0.08,
+            f"{neg.shape[0]}",
+            horizontalalignment="center",
+            verticalalignment="center",
+        )
+
+    # save figure
+    plt.savefig(
+        output_dir / f"conv2d_{new_method_name}_vs_{old_method_name}.png",
+        bbox_inches="tight",
+        dpi=300,
+    )
+    plt.close()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Parse CSV with performance results and summarize them into graphs."
+    )
+
+    parser.add_argument("CSV_Results", type=str, help="Path to the output CSV file.")
+    parser.add_argument("Output_Dir", type=str, help="Path to directory to store outputs.")
+    parser.add_argument(
+        "--include-only-conv-types",
+        nargs="+",
+        type=str,
+        help="Only include the specified convolution types",
+        choices=[
+            "strided",
+            "pointwise",
+            "depthwise",
+            "grouped",
+            "dilated",
+            "transposed",
+        ],
+        default=None,
+    )
+    parser.add_argument(
+        "--exclude-conv-types",
+        nargs="+",
+        type=str,
+        help="List of convolution types to exclude",
+        choices=[
+            "strided",
+            "pointwise",
+            "depthwise",
+            "grouped",
+            "dilated",
+            "transposed",
+        ],
+    )
+
+    parser.add_argument(
+        "--old-method",
+        type=str,
+        help="Set old method for speedup comparison. If not set, all methods will be compared",
+    )
+    parser.add_argument(
+        "--new-method",
+        type=str,
+        help="Set new method for speedup comparison. If not set, all methods will be compared.",
+    )
+
+    args = parser.parse_args()
+
+    csv_results = Path(args.CSV_Results)
+    output_dir = Path(args.Output_Dir)
+    exclude_conv_types = args.exclude_conv_types
+    include_only_conv_types = args.include_only_conv_types
+    old_method = args.old_method
+    new_method = args.new_method
+
+    # Check if csv file exists
+    if (not csv_results.exists()) or (not csv_results.is_file()):
+        print("CSV with results not found.", file=sys.stderr)
+        sys.exit(-1)
+
+    # Check if output dir exists
+    if (not output_dir.exists()) or (not output_dir.is_dir()):
+        print("Output directory not found.", file=sys.stderr)
+        sys.exit(-1)
+
+    df = pd.read_csv(csv_results, header=0, index_col=False)
+
+    # Filter convs if needed
+    num_columns = len(df.columns)
+    df = split_parameters(df)
+    df = include_only_in_df(df, include_only_conv_types)
+    df = exclude_from_df(df, exclude_conv_types)
+    df = df.iloc[:, :num_columns]
+
+    methods = [col.replace("mean_time_", "") for col in df.columns if "mean_time" in col]
+
+    # Check if both methods are present
+    if old_method and f"mean_time_{old_method}" not in df.columns:
+        print(f"Method {old_method} not found in results.", file=sys.stderr)
+        print(f"Available methods: {methods}", file=sys.stderr)
+        sys.exit(-1)
+    if new_method and f"mean_time_{new_method}" not in df.columns:
+        print(f"Method {new_method} not found in results.", file=sys.stderr)
+        print(f"Available methods: {methods}", file=sys.stderr)
+        sys.exit(-1)
+
+    if old_method and new_method:
+        compare_methods(df, old_method, new_method)
+    elif old_method:
+        for method in methods:
+            if method == old_method:
+                continue
+            compare_methods(df, old_method, method)
+    elif new_method:
+        for method in methods:
+            if method == new_method:
+                continue
+            compare_methods(df, method, new_method)
+    else:
+        for method1, method2 in itertools.combinations(methods, 2):
+            compare_methods(df, method1, method2)
--- single-conv/scripts/summarize_performance.py
+++ single-conv/scripts/summarize_performance.py
@@ -2,186 +2,12 @@
 
 import argparse
 import sys
-from math import ceil, floor, log10
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 import pandas as pd
-from filter_csv import exclude_from_df, include_only_in_df, split_parameters
 
 
-# Saves a csv with results and produces an speedup graph
-def compare_methods(joined_results, old_method_name, new_method_name):
-
-    # Remove rows where an error occurred in either method
-    joined_results = joined_results.loc[
-        (joined_results["error_occurred_" + old_method_name] == False)
-        & (joined_results["error_occurred_" + new_method_name] == False)
-    ]
-
-    speedup_results = pd.DataFrame()
-    speedup_results["conv_parameters"] = joined_results["conv_parameters"]
-
-    # Compute speedup
-    speedup_results["speedup"] = (
-        joined_results["mean_time_" + old_method_name]
-        - joined_results["mean_time_" + new_method_name]
-    ) / joined_results["mean_time_" + new_method_name]
-    speedup_results = speedup_results.sort_values(by="speedup", ascending=False)
-
-    # Save resulst to csv
-    speedup_results.to_csv(
-        output_dir / f"conv2d_{old_method_name}_vs_{new_method_name}.csv", index=False
-    )
-
-    speedup = speedup_results["speedup"]
-    num_points = speedup.shape[0]
-
-    inflection = num_points
-    for i in range(0, num_points - 1):
-        if speedup.iloc[i] > 0 and speedup.iloc[i + 1] < 0:
-            inflection = i + 0.5
-
-    pos = speedup.loc[lambda x: x >= 0].reset_index(drop=True)
-    neg = speedup.loc[lambda x: x < 0].reset_index(drop=True)
-
-    fig, ax = plt.subplots()
-
-    # barplot
-    ax.bar(pos.index, pos, color="#2c7bb6")
-    ax.bar(
-        range(pos.shape[0], pos.shape[0] + neg.shape[0], 1), neg.values, color="#d7191c"
-    )
-
-    # boxplot
-    _, x_max = ax.get_xlim()
-    ax.set_xlim((-x_max * 0.05, num_points + x_max * 0.05))
-    ax.boxplot(
-        [pos, neg],
-        showfliers=False,
-        positions=[-x_max * 0.025, num_points + x_max * 0.025],
-        widths=x_max * 0.02,
-    )
-
-    ax.set_ylabel("Speedup/Slowdown")
-    ax.set_xlabel("Convolutions Layers")
-    ax.set_xticks([0, inflection, num_points], [0, int(inflection), num_points])
-
-    y_factor = 0.1
-    y_min, y_max = ax.get_ylim()
-    if y_min < 0:
-        relative_y = min(-y_min * y_factor, y_max * y_factor)
-    else:
-        relative_y = y_max * y_factor
-
-    y_total = y_max - y_min
-
-    ax.hlines(-y_total * 0.05, 1, inflection, "#2c7bb6")
-    ax.vlines(
-        1, -y_total * 0.05 - y_total * 0.01, -y_total * 0.05 + y_total * 0.01, "#2c7bb6"
-    )
-    ax.vlines(
-        inflection,
-        -y_total * 0.05 - y_total * 0.01,
-        -y_total * 0.05 + y_total * 0.01,
-        "#2c7bb6",
-    )
-    ax.text(
-        (inflection / 2),
-        -y_total * 0.08,
-        f"{pos.shape[0]}",
-        horizontalalignment="center",
-        verticalalignment="center",
-    )
-
-    if neg.shape[0] != 0:
-        ax.hlines(y_total * 0.05, inflection, num_points, "#d7191c")
-        ax.vlines(
-            inflection,
-            y_total * 0.05 - y_total * 0.01,
-            y_total * 0.05 + y_total * 0.01,
-            "#d7191c",
-        )
-        ax.vlines(
-            num_points,
-            y_total * 0.05 - y_total * 0.01,
-            y_total * 0.05 + +y_total * 0.01,
-            "#d7191c",
-        )
-        ax.text(
-            ((num_points + inflection) / 2),
-            y_total * 0.08,
-            f"{neg.shape[0]}",
-            horizontalalignment="center",
-            verticalalignment="center",
-        )
-
-    # save figure
-    plt.savefig(
-        output_dir / f"conv2d_{old_method_name}_vs_{new_method_name}.png",
-        bbox_inches="tight",
-        dpi=300,
-    )
-    plt.close()
-
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(
-        description="Parse CSV with results and summarize them into graphs."
-    )
-
-    parser.add_argument("CSV_Results", type=str, help="Path to the output CSV file.")
-    parser.add_argument(
-        "Output_Dir", type=str, help="Path to directory to store outputs."
-    )
-    parser.add_argument(
-        "--include-only-conv-type",
-        type=str,
-        help="Only include the specified convolution type",
-        choices=[
-            "strided",
-            "pointwise",
-            "depthwise",
-            "grouped",
-            "dilated",
-            "transposed",
-        ],
-        default=None,
-    )
-    parser.add_argument(
-        "--exclude-conv-types",
-        nargs="+",
-        type=str,
-        help="List of convolution types to exclude",
-        choices=[
-            "strided",
-            "pointwise",
-            "depthwise",
-            "grouped",
-            "dilated",
-            "transposed",
-        ],
-    )
-
-    args = parser.parse_args()
-
-    csv_results = Path(args.CSV_Results)
-    output_dir = Path(args.Output_Dir)
-    exclude_conv_types = args.exclude_conv_types
-    include_only_conv_type = args.include_only_conv_type
-
-    # Check if csv file exists
-    if (not csv_results.exists()) or (not csv_results.is_file()):
-        print("CSV with results not found.", file=sys.stderr)
-        sys.exit(-1)
-
-    # Check if output dir exists
-    if (not output_dir.exists()) or (not output_dir.is_dir()):
-        print("Output directory not found.", file=sys.stderr)
-        sys.exit(-1)
-
-    df = pd.read_csv(csv_results, header=0, index_col=False)
+def summarize_results(df: pd.DataFrame, output_dir):
 
     # Split the 'name' column into 'conv_type' and 'conv_parameters'
     df[["conv_type", "conv_parameters"]] = df["name"].str.split(" ", n=1, expand=True)
@@ -209,6 +35,11 @@ if __name__ == "__main__":
 
     # Join results by 'conv_parameters'
     method_names = list(df_dict.keys())
+
+    if len(method_names) == 1:
+        print("Only one method found. No comparison possible.", file=sys.stderr)
+        sys.exit(-1)
+
     joined_results = pd.merge(
         df_dict[method_names[0]],
         df_dict[method_names[1]].drop(columns=["time_unit"]),
@@ -218,27 +49,41 @@ if __name__ == "__main__":
     )
     for method_name in method_names[2:]:
         joined_results = joined_results.merge(
-            df_dict[method_name]
-            .drop(columns=["time_unit"])
-            .add_suffix("_" + method_name),
+            df_dict[method_name].drop(columns=["time_unit"]).add_suffix("_" + method_name),
             how="inner",
             left_on="conv_parameters",
             right_on="conv_parameters_" + method_name,
             suffixes=(None, None),
         ).drop(columns=["conv_parameters_" + method_name])
 
-    num_columns = len(joined_results.columns)
-    joined_results = split_parameters(joined_results)
-    joined_results = include_only_in_df(joined_results, include_only_conv_type)
-    joined_results = exclude_from_df(joined_results, exclude_conv_types)
-    joined_results = joined_results.iloc[:, :num_columns]
-
     # Save joined results
-    joined_results.to_csv(output_dir / f"results.csv", index=False)
+    joined_results.to_csv(output_dir / f"performance-results.csv", index=False)
 
-    # These tuples represent the comparisons that will be made into graphs
-    # comparisons = [("Im2col", "Yaconv"), ("Im2col", "ZeroCopy"), ("Yaconv", "ZeroCopy")]
-    comparisons = [("OneDNN_any", "ZeroCopy_jit")]
 
-    for old_method, new_method in comparisons:
-        compare_methods(joined_results, old_method, new_method)
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Parse CSV with performance logs and summarize them into another csv."
+    )
+
+    parser.add_argument(
+        "CSV_Input", type=str, help="Path to the input CSV file with performance logs."
+    )
+    parser.add_argument("Output_Dir", type=str, help="Path to directory to store outputs.")
+
+    args = parser.parse_args()
+
+    csv_input = Path(args.CSV_Input)
+    output_dir = Path(args.Output_Dir)
+
+    # Check if csv file exists
+    if (not csv_input.exists()) or (not csv_input.is_file()):
+        print("CSV with results not found.", file=sys.stderr)
+        sys.exit(-1)
+
+    # Check if output dir exists
+    if (not output_dir.exists()) or (not output_dir.is_dir()):
+        print("Output directory not found.", file=sys.stderr)
+
+    df = pd.read_csv(csv_input, header=0, index_col=False)
+    summarize_results(df, output_dir)
