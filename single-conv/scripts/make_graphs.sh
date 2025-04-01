7d56ac28fe1695877aec6b8a9c97181d2eb27bc6
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2025-04-01 11:35:06 -0600

    Add correctness results to make_graphs

--- single-conv/scripts/make_graphs.sh
+++ single-conv/scripts/make_graphs.sh
@@ -9,11 +9,15 @@ if [[ ! -d $INPUT_DIR || -z $INPUT_DIR ]]; then
     exit 1
 fi
 
+mkdir -p "$INPUT_DIR"/correctness/1-thread "$INPUT_DIR"/correctness/8-threads
+./summarize_correctness.py "$INPUT_DIR"/cdol01-correctness-1-thread.csv "$INPUT_DIR"/correctness/1-thread
+./summarize_correctness.py "$INPUT_DIR"/cdol01-correctness-8-threads.csv "$INPUT_DIR"/correctness/8-threads
+
 mkdir -p "$INPUT_DIR"/general-graphs/1-thread "$INPUT_DIR"/general-graphs/8-threads
 mkdir -p "$INPUT_DIR"/paper-graphs/1-thread "$INPUT_DIR"/paper-graphs/8-threads
 
-./summarize_performance.py "$INPUT_DIR"/cdol01-performance-1-thread.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/general-graphs/1-thread --clip-pos --clip-neg --plot-type log2_speedup --preset-comparisons --incorrect-convs "$INPUT_DIR"/correctness/1-thread/incorrect-convolutions.csv
-./summarize_performance.py "$INPUT_DIR"/cdol01-performance-8-threads.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/general-graphs/8-threads --clip-pos --clip-neg --plot-type log2_speedup --preset-comparisons --exclude pointwise
+./summarize_performance.py "$INPUT_DIR"/cdol01-performance-1-thread.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/general-graphs/1-thread --clip-pos --clip-neg --plot-type log2_speedup --preset-comparisons --incorrect-convs "$INPUT_DIR"/correctness/1-thread/incorrect-convolutions.csv --exclude pointwise
+./summarize_performance.py "$INPUT_DIR"/cdol01-performance-8-threads.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/general-graphs/8-threads --clip-pos --clip-neg --plot-type log2_speedup --preset-comparisons --incorrect-convs "$INPUT_DIR"/correctness/8-threads/incorrect-convolutions.csv --exclude pointwise
 
 ./summarize_performance.py "$INPUT_DIR"/general-graphs/1-thread/performance-results.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/paper-graphs/1-thread --plot-type log2_speedup --clip-pos --clip-neg --old-method Yaconv --new-method ZeroCopy_jit --already-merged
 ./summarize_performance.py "$INPUT_DIR"/general-graphs/1-thread/performance-results.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/paper-graphs/1-thread --plot-type log2_speedup --clip-pos --clip-neg --old-method Im2col --new-method ZeroCopy_jit --exclude grouped --already-merged
