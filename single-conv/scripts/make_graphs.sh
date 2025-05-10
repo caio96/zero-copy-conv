18c8f1037e4fabd769e7c663a108fb0a6b52951d
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2025-05-10 00:53:12 -0300

    Update make graphs to generate yaconv-zconv-blis graph

--- single-conv/scripts/make_graphs.sh
+++ single-conv/scripts/make_graphs.sh
@@ -14,7 +14,7 @@ mkdir -p "$INPUT_DIR"/correctness/1-thread "$INPUT_DIR"/correctness/8-threads
 ./summarize_correctness.py "$INPUT_DIR"/cdol01-correctness-8-threads.csv "$INPUT_DIR"/correctness/8-threads
 
 mkdir -p "$INPUT_DIR"/general-graphs/1-thread "$INPUT_DIR"/general-graphs/8-threads
-mkdir -p "$INPUT_DIR"/paper-graphs/1-thread "$INPUT_DIR"/paper-graphs/8-threads
+mkdir -p "$INPUT_DIR"/paper-graphs/1-thread "$INPUT_DIR"/paper-graphs/1-thread-zconv-blis "$INPUT_DIR"/paper-graphs/8-threads
 
 ./summarize_performance.py "$INPUT_DIR"/cdol01-performance-1-thread.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/general-graphs/1-thread --clip-pos --clip-neg --plot-type log2_speedup --preset-comparisons --incorrect-convs "$INPUT_DIR"/correctness/1-thread/incorrect-convolutions.csv --exclude pointwise
 ./summarize_performance.py "$INPUT_DIR"/cdol01-performance-8-threads.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/general-graphs/8-threads --clip-pos --clip-neg --plot-type log2_speedup --preset-comparisons --incorrect-convs "$INPUT_DIR"/correctness/8-threads/incorrect-convolutions.csv --exclude pointwise
@@ -22,6 +22,9 @@ mkdir -p "$INPUT_DIR"/paper-graphs/1-thread "$INPUT_DIR"/paper-graphs/8-threads
 ./summarize_performance.py "$INPUT_DIR"/general-graphs/1-thread/performance-results.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/paper-graphs/1-thread --plot-type log2_speedup --clip-pos --clip-neg --old-method Yaconv --new-method ZeroCopy_jit --already-merged
 ./summarize_performance.py "$INPUT_DIR"/general-graphs/1-thread/performance-results.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/paper-graphs/1-thread --plot-type log2_speedup --clip-pos --clip-neg --old-method Im2col --new-method ZeroCopy_jit --exclude grouped --already-merged
 ./summarize_performance.py "$INPUT_DIR"/general-graphs/1-thread/performance-results.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/paper-graphs/1-thread --plot-type log2_speedup --clip-pos --clip-neg --old-method LibTorch --new-method LibTorch_ZeroCopy2D_HWIO_TransformOutput --include torch-heuristic --already-merged
+
+# copy conv_layers_yaconv_supported.csv from data/ to the input dir
+./summarize_performance.py "$INPUT_DIR"/cdol01-performance-1-thread-zconv-blis.csv "$INPUT_DIR"/conv_layers_yaconv_supported.csv "$INPUT_DIR"/paper-graphs/1-thread-zconv-blis --plot-type log2_speedup --clip-pos --clip-neg --old-method Yaconv --new-method ZeroCopy_blis
 
 ./summarize_performance.py "$INPUT_DIR"/general-graphs/8-threads/performance-results.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/paper-graphs/8-threads --plot-type log2_speedup --clip-pos --clip-neg --old-method Im2col --new-method ZeroCopy_jit --already-merged
 ./summarize_performance.py "$INPUT_DIR"/general-graphs/8-threads/performance-results.csv "$INPUT_DIR"/all.csv "$INPUT_DIR"/paper-graphs/8-threads --plot-type log2_speedup --clip-pos --clip-neg --old-method LibTorch --new-method LibTorch_ZeroCopy2D_HWIO_TransformOutput --include torch-heuristic --already-merged
